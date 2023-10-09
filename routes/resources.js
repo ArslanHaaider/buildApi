8e2f672d689da9e7fe7d3bf0a74906f97b728943
AslanHaaider <aslanhaider707@gmail.com>
2023-10-09 08:47:44 +0500

    made all routes

--- routes/resources.js
+++ routes/resources.js
@@ -1,8 +1,10 @@
+let resources = require('../controller/resources/resources');
 const express = require('express');
 let router = express.Router();
 
-router.get('/',(req,res)=>{
-  res.send('router working')
-});
-
+router.get('/:id',resources.giveResources);
+router.get('/', resources.getAllResources);
+router.post('/createResource', resources.createResource);
+router.delete('/deleteResource/:id', resources.deleteResource);
+router.put('/updateResource/:id', resources.updateResource);
 module.exports = router;
